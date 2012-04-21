fedd7e07b218105b79d25efffc4efd82cddcfd28
nnmware <nnmware@gmail.com>
2012-04-22 02:28:24 +1100

    Booking amount

--- core/templatetags/booking_tags.py
+++ core/templatetags/booking_tags.py
@@ -3,7 +3,7 @@ import re
 
 from django.template import Library
 from django.utils.translation import ugettext_lazy as _
-from nnmware.apps.booking.models import Hotel, TWO_STAR, THREE_STAR, FOUR_STAR, FIVE_STAR
+from nnmware.apps.booking.models import Hotel, TWO_STAR, THREE_STAR, FOUR_STAR, FIVE_STAR, HotelOption
 
 
 register = Library()
@@ -28,4 +28,11 @@ def five_star_count(city=None):
     result = Hotel.objects.filter(starcount=FIVE_STAR).count()
     return result
 
+@register.assignment_tag
+def search_sticky_options():
+    return HotelOption.objects.filter(sticky_in_search=True).order_by('order_in_list')
+
+@register.assignment_tag
+def search_options():
+    return HotelOption.objects.filter(sticky_in_search=False, in_search=True).order_by('order_in_list')
 
