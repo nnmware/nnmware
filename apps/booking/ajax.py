fe8de78134b5c40a94bd875c7634f23e1fb5a9fe
nnmware <nnmware@gmail.com>
2012-05-15 21:20:11 +1100

    Map cleaned

--- apps/booking/ajax.py
+++ apps/booking/ajax.py
@@ -196,7 +196,7 @@ def hotels_in_city(request):
         for hotel in Hotel.objects.filter(city=city).order_by('starcount'):
             answer = {'name':hotel.get_name, 'latitude':hotel.latitude,'url':hotel.get_absolute_url(),
                       'address':hotel.address,'id':hotel.pk,'starcount':hotel.starcount,
-                      'img':make_thumbnail(hotel.main_image,width=113,height=75),
+                      'img':make_thumbnail(hotel.main_image,width=113,height=75,aspect=1),
                       'longitude':hotel.longitude, 'starcount_name':hotel.get_starcount_display()}
             results.append(answer)
         payload = {'success': True, 'hotels':results}
