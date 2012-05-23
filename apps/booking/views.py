8159d977eba80ddd4acea3cfcbd752412039beeb
nnmware <nnmware@gmail.com>
2012-05-23 22:04:46 +1100

    Remake search

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -687,6 +687,16 @@ class RequestAdminAdd(CurrentUserSuperuser, TemplateView):
 class HotelMainPage(TemplateView):
     template_name = 'hotels/intro.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HotelMainPage, self).get_context_data(**kwargs)
+        city = City.objects.get(slug='spb')
+        result_spb = Hotel.objects.filter(city=city).count()
+        city = City.objects.get(slug='moscow')
+        result_moscow = Hotel.objects.filter(city=city).count()
+        context['hotels_moscow'] = result_moscow
+        context['hotels_spb'] = result_spb
+        return context
+
 class BookingHotelDetail(CurrentUserHotelBookingAccess, DetailView):
     model = Booking
     slug_field = 'uuid'
