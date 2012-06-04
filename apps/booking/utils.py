e055bed3c3253b667226b1351783436f9c3700ef
nnmware <nnmware@gmail.com>
2012-06-04 21:17:36 +1100

    Mail notices for booking

--- apps/booking/utils.py
+++ apps/booking/utils.py
@@ -21,3 +21,14 @@ def booking_new_client_mail(booking, username=''):
         message = render_to_string('booking/new_client.txt', mail_dict)
         send_mail(subject=subject, message=message, from_email=settings.EMAIL_HOST_USER,
             recipient_list=[booking.email])
+
+def booking_new_hotel_mail(booking):
+    recipients = settings.MANAGERS
+    if booking.hotel.email:
+        mail_dict = {'booking': booking,
+                     'site_name': settings.SITENAME, 'username': username}
+        subject = render_to_string('booking/new_client_subject.txt', mail_dict)
+        subject = ''.join(subject.splitlines())
+        message = render_to_string('booking/new_client.txt', mail_dict)
+        send_mail(subject=subject, message=message, from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[booking.email])
