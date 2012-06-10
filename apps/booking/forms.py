fb8620732fa1f8ba73d54a87a38a184ce936946a
nnmware <nnmware@gmail.com>
2012-06-11 10:47:33 +1100

    fixed Booking

--- apps/booking/forms.py
+++ apps/booking/forms.py
@@ -28,6 +28,7 @@ class CabinetTermsForm(forms.ModelForm):
     condition_cancellation = forms.CharField(widget=forms.Textarea(attrs={'class' : 'wide','rows':'5'}))
     paid_services = forms.CharField(widget=forms.Textarea(attrs={'class' : 'wide','rows':'5'}))
     time_on = forms.CharField(widget=AdminTimeWidget())
+    time_off = forms.CharField(widget=AdminTimeWidget())
 
     class Meta:
         model = Hotel
