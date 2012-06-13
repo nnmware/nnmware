a9ede85515cdf3704a8c63cc919016af1c938f23
nnmware <nnmware@gmail.com>
2012-06-14 09:08:01 +1100

    Remove global variable

--- core/middleware.py
+++ core/middleware.py
@@ -1,24 +1,7 @@
+import json
+from datetime import datetime
 from django.conf import settings
 from django.contrib import messages
-from datetime import datetime
-import json
-from nnmware.core.utils import get_message_dict
-
-from threading import local
-
-_thread_locals = local()
-
-
-def get_request():
-    return getattr(_thread_locals, 'request', None)
-
-
-class ThreadLocalsMiddleware(object):
-    """Middleware that saves request in thread local storage"""
-
-    def process_request(self, request):
-        _thread_locals.request = request
-
 
 def get_page(self):
     """
