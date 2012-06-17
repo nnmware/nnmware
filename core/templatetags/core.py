e0738945ec3d73da95fb78e2770542412f903ca7
nnmware <nnmware@gmail.com>
2012-06-18 04:54:20 +1100

    Core optimized

--- core/templatetags/core.py
+++ core/templatetags/core.py
@@ -102,22 +102,23 @@ def paginator(context):
     view.
     """
     num_pages= context["paginator"].num_pages
+    curr_page_num = context["page"].number
     in_leading_range = in_trailing_range = False
     pages_outside_leading_range = pages_outside_trailing_range = range(0)
 
     if num_pages <= LEADING_PAGE_RANGE_DISPLAYED:
         in_leading_range = in_trailing_range = True
         page_numbers = [n for n in range(1, num_pages + 1) if n > 0 and n <= num_pages]
-    elif num_pages <= LEADING_PAGE_RANGE:
+    elif curr_page_num <= LEADING_PAGE_RANGE:
         in_leading_range = True
         page_numbers = [n for n in range(1, LEADING_PAGE_RANGE_DISPLAYED + 1) if n > 0 and n <= num_pages]
         pages_outside_leading_range = [n + num_pages for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
-    elif num_pages > (num_pages - TRAILING_PAGE_RANGE):
+    elif curr_page_num > (num_pages - TRAILING_PAGE_RANGE):
         in_trailing_range = True
         page_numbers = [n for n in range(num_pages - TRAILING_PAGE_RANGE_DISPLAYED + 1, num_pages + 1) if n > 0 and n <= num_pages]
         pages_outside_trailing_range = [n + 1 for n in range(0, NUM_PAGES_OUTSIDE_RANGE)]
     else:
-        page_numbers = [n for n in range(num_pages - ADJACENT_PAGES, num_pages + ADJACENT_PAGES + 1) if n > 0 and n <= num_pages]
+        page_numbers = [n for n in range(curr_page_num - ADJACENT_PAGES, curr_page_num + ADJACENT_PAGES + 1) if n > 0 and n <= num_pages]
         pages_outside_leading_range = [n + num_pages for n in range(0, -NUM_PAGES_OUTSIDE_RANGE, -1)]
         pages_outside_trailing_range = [n + 1 for n in range(0, NUM_PAGES_OUTSIDE_RANGE)]
     return {
