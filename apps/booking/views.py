576e8c5f05f1d0cb6443ec25a9ce649738319dbe
nnmware <nnmware@gmail.com>
2012-06-21 01:43:40 +1100

    Core optimized

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -694,7 +694,7 @@ class UserCabinet(CurrentUserCabinetAccess, UpdateView):
 class UserBookings(CurrentUserCabinetAccess, SingleObjectMixin, ListView):
 #    model = Profile
     paginate_by = 5
-    template_name = "usercabinet/bookings_new.html"
+    template_name = "usercabinet/bookings.html"
 
     def get_object(self, queryset=None):
         user = get_object_or_404(User, username=self.kwargs['username'])
