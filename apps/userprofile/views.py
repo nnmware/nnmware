20084a755a4d40b5fe7976182b134664093b492f
nnmware <nnmware@gmail.com>
2012-07-25 02:24:58 +1100

    Signup Forms

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -221,6 +221,7 @@ class LoginView(FormView):
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
         user = authenticate(username=username, password=password)
+        raise
         login(self.request, user)
         return super(LoginView, self).form_valid(form)
 
