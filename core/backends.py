c51b68e4875b93e12c119657a6092cb1db866c0a
nnmware <nnmware@gmail.com>
2012-07-25 02:12:56 +1100

    Signup Forms

--- core/backends.py
+++ core/backends.py
@@ -39,6 +39,27 @@ class EmailAuthBackend(object):
         except User.DoesNotExist:
             return None
 
+class UsernameOrEmailAuthBackend(object):
+
+    def authenticate(self, username=None, password=None):
+        if '@' in username:
+            kwargs = {'email': username}
+        else:
+            kwargs = {'username': username}
+        try:
+            user = User.objects.get(**kwargs)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        """ Get a User object from the user_id. """
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
 
 class AbstractUploadBackend(object):
     BUFFER_SIZE = 10485760  # 10MB
