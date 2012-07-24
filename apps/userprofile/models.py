91130d928658f9955231b89778895ec3c80d6b6e
nnmware <nnmware@gmail.com>
2012-07-25 07:17:56 +1100

    Signup Forms

--- apps/userprofile/models.py
+++ apps/userprofile/models.py
@@ -100,8 +100,8 @@ class Profile(models.Model):
     @property
     def followers(self):
         ctype = ContentType.objects.get_for_model(User)
-        users = Follow.objects.filter(content_type=ctype,object_id=self.user.pk).values_list('user',flat=True)
-        return User.objects.filter(pk__in=users)
+        return Follow.objects.filter(content_type=ctype,object_id=self.user.pk).values_list('user',flat=True)
+#        return User.objects.filter(pk__in=users)
 
 
     def follow_tags_count(self):
