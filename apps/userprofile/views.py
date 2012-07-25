8eae867a9c2718d7ad83529e38600100f50cf2f0
nnmware <nnmware@gmail.com>
2012-07-25 14:56:33 +1100

    Signup Forms

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -189,7 +189,7 @@ class SignupView(AjaxFormMixin, FormView):
     def form_valid(self, form):
         email = form.cleaned_data.get('email')
         username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
+        password = form.cleaned_data.get('password2')
         try:
             e = EmailValidation.objects.get(email=email)
         except :
