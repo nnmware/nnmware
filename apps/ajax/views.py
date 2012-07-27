daf6b9cdd43784459ba03c5d3bf3bcf84cdc34cc
nnmware <nnmware@gmail.com>
2012-07-28 08:39:18 +1100

    Follow ajax functions refactored

--- apps/ajax/views.py
+++ apps/ajax/views.py
@@ -441,8 +441,8 @@ def push_message(request, object_id):
         message_date = msg.sent_at.strftime(settings.COMMENT_DATE_FORMAT)
         payload = {'success': True, 'id':msg.pk, 'username':msg.sender.get_profile().get_name,
                    'username_url':msg.sender.get_profile().get_absolute_url(),
-                   'message':msg.subject, 'avatar_id':avatar_id,
-                   'message_date': message_date}
+                   'message_subject':msg.subject, 'avatar_id':avatar_id,
+                   'message_date': message_date, 'message_body':msg.body }
     except AccessError:
         payload = {'success': False, 'error':_('You are not allowed for send message')}
     except :
