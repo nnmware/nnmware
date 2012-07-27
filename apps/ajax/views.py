4de3e2c568ae7d2183acf6f26f5be045c635eaf4
nnmware <nnmware@gmail.com>
2012-07-28 00:06:52 +1100

    Threaded ajax comments

--- apps/ajax/views.py
+++ apps/ajax/views.py
@@ -224,8 +224,8 @@ def push_user(request, object_id):
                         notice.send(request.user, user=u, verb=_('also now follow'), target=user)
                     else:
                         notice.send(request.user, user=u, verb=_('now follow'), target=user)
-        count = Follow.objects.filter(content_type=ctype, object_id=object_id).count()
-        payload = {'success': True, 'count': count, 'id': user.pk, 'status':status}
+        result = Follow.objects.filter(content_type=ctype, object_id=object_id).count()
+        payload = {'success': True, 'count': result, 'id': user.pk, 'status':status}
     except :
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
