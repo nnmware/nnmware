91ec6be1311a2c6bb3d2ae86d7718931fb2227d9
nnmware <nnmware@gmail.com>
2012-08-04 09:27:41 +1100

    Video app fixed

--- core/models.py
+++ core/models.py
@@ -701,7 +701,7 @@ class Action(MetaLink,MetaIP):
 
     @property
     def target_type(self):
-        return ContentType.objects.get_for_model(self.content_type).model
+        return repr(ContentType.objects.get_for_model(self.content_type))
 
 
     def __unicode__(self):
