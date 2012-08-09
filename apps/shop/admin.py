789abff896835bd95f05d845f00f7e51cc02ddb0
nnmware <nnmware@gmail.com>
2012-08-09 11:52:13 +1100

    Shop base implemented

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -14,7 +14,7 @@ class ProductParameterValueInline(generic.GenericStackedInline):
 
 class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ('created_date','updated_date')
-    list_display = ("name", "category", "created_date",'quantity','amount','shop_pn','vendor_pn')
+    list_display = ("name", "category", 'vendor',"created_date",'quantity','amount','shop_pn','vendor_pn')
     prepopulated_fields = {'slug': ('name',)}
     inlines = [ ProductParameterValueInline, ]
     fieldsets = (
