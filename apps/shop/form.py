f6263a061c678a030d5fb844a4d4f31250975a26
nnmware <nnmware@gmail.com>
2012-08-11 00:37:00 +1100

    version 0.52 of engine

--- apps/shop/form.py
+++ apps/shop/form.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
 from nnmware.apps.shop.models import Product
 
 class EditProductForm(forms.ModelForm):
