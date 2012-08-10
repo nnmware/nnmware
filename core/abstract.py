bb3d2ebcdc3ec92c7dbeedc0f2c2951f1cbc24cd
nnmware <nnmware@gmail.com>
2012-08-10 14:57:13 +1100

    Shop base implemented

--- core/abstract.py
+++ core/abstract.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+# Base abstract classed nnmware(c)2012
+
 from datetime import datetime
+
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 class MetaDate(models.Model):
     created_date = models.DateTimeField(_("Created date"), default=datetime.now())
