89576e3224ff46acd58097ba902b6188eabe4a9d
nnmware <nnmware@gmail.com>
2012-08-12 14:45:10 +1100

    Core models optimized

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
-from django.views.generic.detail import DetailView
+from django.views.generic.detail import DetailView, SingleObjectMixin
 from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 from nnmware.apps.shop.form import EditProductForm
 from nnmware.apps.shop.models import Product, ProductCategory, Basket
 from nnmware.core.data import get_queryset_category
+from nnmware.core.models import JComment
 from nnmware.core.views import CurrentUserSuperuser, AttachedImagesMixin, AjaxFormMixin
 
 
@@ -22,9 +24,22 @@ class ShopAllCategory(ListView):
     template_name = 'shop/product_list.html'
     model = Product
 
-class ProductView(DetailView):
+class ProductDetail(SingleObjectMixin, ListView):
+    # For case-sensitive need UTF8_BIN collation in Slug_Field
+    paginate_by = 20
     template_name = 'shop/product.html'
-    model = Product
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Product, pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        kwargs['object'] = self.object
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        self.object = self.get_object()
+        return JComment.public.get_tree(self.object)
 
 class BasketView(ListView):
     model = Basket
