6521b58354333d0c039cc52071e60accb7217728
nnmware <nnmware@gmail.com>
2012-08-20 21:18:42 +1100

    Core models optimized

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -398,9 +398,15 @@ class SettlementVariant(models.Model):
         verbose_name_plural = _("Settlements Variants")
 
     def __unicode__(self):
-        return _("Settlement -> %(settlement)s in %(room)s :: %(places)s :: %(hotel)s") % {
+        try:
+            return _("Settlement -> %(settlement)s in %(room)s :: %(places)s :: %(hotel)s") % {
             'settlement': self.settlement, 'room': self.room.get_name, 'places':self.room.places,
             'hotel':self.room.hotel.get_name }
+        except :
+            return _("Settlement -> %(settlement)s in %(room)s :: %(places)s :: %(hotel)s") % {
+            'settlement': self.settlement, 'room': self.room.get_name, 'places':self.room.places,
+            'hotel':self.room.hotel }
+            
 
     def current_amount(self):
         result = PlacePrice.objects.filter(settlement=self, date__lte=datetime.now()).order_by('-date')
