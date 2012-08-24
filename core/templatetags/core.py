dc92832ddf86db0be5eb20b29ad47739d4fd0468
nnmware <nnmware@gmail.com>
2012-08-25 05:35:44 +1100

    Core models optimized

--- core/templatetags/core.py
+++ core/templatetags/core.py
@@ -187,7 +187,7 @@ def no_end_slash(value):
 @register.assignment_tag(takes_context=True)
 def basket(context):
     user = context['user']
-    if user.is_authenticated():
+    if user.is_authenticated:
         return Basket.objects.filter(user=user)
     request = context['request']
     if hasattr(request, 'session') and request.session.session_key:
