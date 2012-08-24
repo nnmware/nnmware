7bf0e857b060fcb5510e62b963eab2e5ac4295b0
nnmware <nnmware@gmail.com>
2012-08-25 02:49:13 +1100

    Core models optimized

--- core/data.py
+++ core/data.py
@@ -10,9 +10,9 @@ def get_queryset_category(obj, main_obj, cat_obj, order='-created_date'):
     if obj.kwargs['parent_slugs']:
         parent_slugs = obj.kwargs['parent_slugs']
         parent = cat_obj.objects.all().filter(slug=parent_slugs[:-1])[0].id
-        q = cat_obj.objects.filter(parent=parent).filter(slug=slug)
+        q = cat_obj.objects.get(parent=parent,slug=slug)
     else:
-        q = cat_obj.objects.filter(slug=slug)
+        q = cat_obj.objects.get(slug=slug,parent=None)
     child = q[0].id
     res = main_obj.objects.select_related()
     return res.filter(category=child).order_by(order)
