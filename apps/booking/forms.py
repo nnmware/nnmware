f58f4487d8a875df82153f72f9fd8a695c499c3d
nnmware <nnmware@gmail.com>
2012-09-03 11:05:08 +1100

    Shop module maked

--- apps/booking/forms.py
+++ apps/booking/forms.py
@@ -16,11 +16,13 @@ class CabinetInfoForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'size' : '25'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class' : 'wide','rows':'5'}),required=False)
     schema_transit = forms.CharField(widget=forms.Textarea(attrs={'class' : 'wide','rows':'5'}),required=False)
-    typefood = forms.ChoiceField(widget=forms.RadioSelect)
 
     class Meta:
         model = Hotel
         fields = ('name', 'description', 'schema_transit', 'option','typefood')
+        widgets = {
+            'typefood': forms.RadioSelect(),
+            }
 
 class CabinetTermsForm(forms.ModelForm):
     booking_terms = forms.CharField(widget=forms.Textarea(attrs={'class' : 'wide','rows':'5'}),required=False)
