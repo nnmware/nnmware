37342913584d61851a1790e9f6850e1aa1947037
nnmware <nnmware@gmail.com>
2012-09-05 12:56:17 +1100

    Shop module maked

---
+++ apps/booking/management/commands/check_correct_hotel_info.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta, datetime
+
+from django.core.management.base import BaseCommand
+from nnmware.apps.booking.models import Hotel, Availability
+
+class Command(BaseCommand):
+    help = 'Check correct info in hotel cabinet'
+
+    def handle(self, *args, **options):
+        for hotel in Hotel.objects.all():
+            for room in hotel.room_set.all():
+                avail = Availability.objects.filter(room=room,date__range=(datetime.now(), datetime.now()+timedelta(days=14))).count():
+                print avail
+
