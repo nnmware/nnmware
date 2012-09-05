bc59524094c16bbf39cb0680064b87be08cf852d
nnmware <nnmware@gmail.com>
2012-09-05 13:17:32 +1100

    Shop module maked

--- apps/booking/management/commands/check_correct_hotel_info.py
+++ apps/booking/management/commands/check_correct_hotel_info.py
@@ -2,7 +2,7 @@
 from datetime import timedelta, datetime
 
 from django.core.management.base import BaseCommand
-from nnmware.apps.booking.models import Hotel, Availability
+from nnmware.apps.booking.models import Hotel, Availability, SettlementVariant
 
 class Command(BaseCommand):
     help = 'Check correct info in hotel cabinet'
@@ -13,4 +13,7 @@ class Command(BaseCommand):
                 avail = Availability.objects.filter(room=room,date__range=(datetime.now(), datetime.now()+timedelta(days=13))).count()
                 if avail < 14:
                     print avail, room.get_name
+                for settlement in SettlementVariant.objects.filter(room=room, enabled=True):
+                    if settlement.current_amount() is 0:
+                        print settlement.room.get_name, settlement.settlement
 
