2c590828afe3b1ee23b2587d731e117ef6284abe
nnmware <nnmware@gmail.com>
2012-09-05 14:27:57 +1100

    Shop module maked

--- apps/booking/management/commands/check_correct_hotel_info.py
+++ apps/booking/management/commands/check_correct_hotel_info.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta, datetime
+from django.conf import settings
 from django.utils.translation import ugettext as _
 from django.utils.translation import activate
 
 from django.core.management.base import BaseCommand
 from nnmware.apps.booking.models import Hotel, Availability, SettlementVariant
+from nnmware.core.utils import send_template_mail
 
 class Command(BaseCommand):
     help = 'Check correct info in hotel cabinet'
@@ -23,5 +25,11 @@ class Command(BaseCommand):
                         settlement_err = [room.get_name,_('Not filled price for %s-placed settlement') % settlement.settlement  ]
                         result.append(settlement_err)
             if len(result) > 0:
-                for item in result:
-                    print item[0], item[1]
+                recipients = settings.BOOKING_MANAGERS
+                mail_dict = {'hotel': hotel, 'site_name': settings.SITENAME,'items':result}
+                subject = 'booking/err_hotel_subject.txt'
+                body = 'booking/err_hotel.txt'
+                send_template_mail(subject,body,mail_dict,recipients)
+#
+#        for item in result:
+#                    print item[0], item[1]
