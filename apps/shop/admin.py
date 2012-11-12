9cd7eadaa6f01faa35894447be533be459606380
nnmware <nnmware@gmail.com>
2012-11-13 06:38:18 +1100

    module "Userprofile" start removing(django 1.5 deprecated)

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -14,12 +14,12 @@ class ProductParameterValueInline(generic.GenericStackedInline):
 
 class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ('created_date','updated_date')
-    list_display = ("name", "category", 'vendor',"created_date",'quantity','amount','shop_pn','vendor_pn')
+    list_display = ("name", "category", 'vendor',"created_date",'quantity','amount','shop_pn','vendor_pn','visible')
     prepopulated_fields = {'slug': ('name',)}
     inlines = [ ProductParameterValueInline, ]
     fieldsets = (
         (_("Product"), {"fields": [('name','category'),
-            ('slug','vendor','deleted'),
+            ('slug','vendor','visible'),
             ('amount','quantity'),
             ('description',),
             ('color',"created_date",'updated_date'),
