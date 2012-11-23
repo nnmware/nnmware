1af40a8c572d03f864252deb6447484cb6ff8f0c
nnmware <nnmware@gmail.com>
2012-11-23 18:40:17 +1100

    Core fixes

--- apps/address/admin.py
+++ apps/address/admin.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class CityAdmin(admin.ModelAdmin):
-    list_display = ('name','region','country','slug')
+    list_display = ('name','region','country','slug','lat','lon')
     search_fields = ('name',)
     fieldsets = (
         (_("City"), {"fields": [("name","slug"),
