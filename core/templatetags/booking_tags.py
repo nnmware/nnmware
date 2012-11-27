798ac2a635cd5848673e56c1ba3b941319d7d046
nnmware <nnmware@gmail.com>
2012-11-28 02:31:11 +1100

    Booking module change auth method on new in Django 1.5

--- core/templatetags/booking_tags.py
+++ core/templatetags/booking_tags.py
@@ -4,7 +4,7 @@ from django.db.models import Min, Max
 
 from django.template import Library
 from django.template.defaultfilters import stringfilter
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from nnmware.apps.address.models import City
 from nnmware.apps.booking.models import Hotel, TWO_STAR, THREE_STAR, FOUR_STAR, FIVE_STAR, \
     HotelOption, MINI_HOTEL, PlacePrice, Availability, HOSTEL, Discount
