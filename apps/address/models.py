817740788b6301b7dd5bb9b844ad092c2728f887
nnmware <nnmware@gmail.com>
2012-11-30 01:44:14 +1100

    0.54 released

--- apps/address/models.py
+++ apps/address/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation.trans_real import get_language
 from nnmware.core.fields import std_text_field
-from nnmware.core.maps import Geocoder, osm_geocoder
+from nnmware.core.maps import osm_geocoder
 from nnmware.core.abstract import AbstractName
 
 class Address(AbstractName):
