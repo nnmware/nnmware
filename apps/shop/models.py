5e345ecfdb40053514cd251e1e7cbf67b3e7b679
nnmware <nnmware@gmail.com>
2012-12-17 19:36:25 +1100

    Next project implemented

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -238,6 +238,17 @@ class Order(AbstractDate):
             result += ' ' + self.middle_name
         return result
 
+    @property
+    def fio(self):
+        result = ''
+        if self.last_name <> '' and self.last_name is not None:
+            result += self.last_name
+        if self.first_name <> '' and self.first_name is not None:
+            result += ' ' + self.first_name[0]
+        if self.middle_name <> '' and self.middle_name is not None:
+            result += ' ' + self.middle_name[0]
+        return result
+
 
 class OrderItem(MoneyBase):
     """
