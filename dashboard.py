325f93a43e7ff5adec40fcb8748ae5fad1d2fc3d
nnmware <nnmware@gmail.com>
2012-12-20 20:10:51 +1100

    Concrete refactored

--- dashboard.py
+++ dashboard.py
@@ -49,6 +49,18 @@ class NnmwareDashboard(Dashboard):
                 	    ),
                 ),
                 modules.AppList(
+                    _('Dossier'),
+                    column=1,
+                    css_classes=('grp-collapse grp-closed',),
+                    models=('nnmware.apps.dossier.*',),
+                ),
+                modules.AppList(
+                    _('Business'),
+                    column=1,
+                    css_classes=('grp-collapse grp-closed',),
+                    models=('nnmware.apps.business.*',),
+                ),
+                modules.AppList(
                     _('Administration Module'),
                     column=1,
                     css_classes=('grp-collapse grp-closed',),
