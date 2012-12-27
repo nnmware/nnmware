ecca3fd96dee5417fb88b638b3aa409219f160a5
nnmware <nnmware@gmail.com>
2012-12-28 10:16:40 +1100

    Shop module optimized

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -403,7 +403,6 @@ class SpecialOffer(AbstractOffer):
         super(SpecialOffer, self).save(*args, **kwargs)
 
 
-#    @permalink
+    @permalink
     def get_absolute_url(self):
-#        return "special_offer", (), {'slug': self.slug}
-        return reverse("special_offer", kwargs={'slug': self.slug})
+        return "special_offer", (), {'pk':self.pk}
