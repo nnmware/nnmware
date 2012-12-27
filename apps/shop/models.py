5ee73c54da096ab5cbf3524cae96af39c6cc4a65
nnmware <nnmware@gmail.com>
2012-12-27 21:44:35 +1100

    Shop module optimized

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -396,4 +396,4 @@ class SpecialOffer(AbstractOffer):
 
     @permalink
     def get_absolute_url(self):
-        return "special_offer", (), {'slug': self.slug}
+        return reverse("special_offer", (), {'slug': self.slug})
