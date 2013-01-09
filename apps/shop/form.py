be68de10b8c4f2ac82fb7c2bcc5bc64bdf67492e
nnmware <nnmware@gmail.com>
2013-01-09 18:37:56 +1100

    Shop module was refactored(orders block)

--- apps/shop/form.py
+++ apps/shop/form.py
@@ -47,7 +47,7 @@ class OrderTrackingForm(forms.ModelForm):
         fields = ('tracknumber','cargoservice')
 
 class AnonymousOrderAddForm(forms.ModelForm):
-    delivery = forms.ModelChoiceField(queryset=DeliveryMethod.objects.all())
+    delivery = forms.ModelChoiceField(queryset=DeliveryMethod.objects.all(), required=False)
 
     class Meta:
         model = Order
