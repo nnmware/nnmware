070177326fb2730b5a981697acc3dedc24267a2c
nnmware <nnmware@gmail.com>
2013-02-16 11:04:30 +1100

    PEP8 fixes

--- apps/booking/ajax.py
+++ apps/booking/ajax.py
@@ -11,7 +11,8 @@ from django.core.urlresolvers import reverse
 from django.db.models.aggregates import Avg
 from django.utils.translation import ugettext_lazy as _
 from nnmware.apps.address.models import City
-from nnmware.apps.booking.models import SettlementVariant, PlacePrice, Room, Availability, Hotel, RequestAddHotel, Review, Booking, PaymentMethod, Discount
+from nnmware.apps.booking.models import SettlementVariant, PlacePrice, Room, Availability, Hotel, RequestAddHotel, \
+    Review, Booking, PaymentMethod, Discount
 from nnmware.apps.money.models import Currency
 import time
 from nnmware.core.imgutil import make_thumbnail
@@ -20,11 +21,14 @@ from nnmware.core.utils import convert_to_date
 from nnmware.core.ajax import AjaxLazyAnswer
 from django.views.decorators.cache import never_cache
 
+
 class UserNotAllowed(Exception):
     pass
 
+
 class RatesError(Exception):
     pass
+
 
 @never_cache
 def room_rates(request):
@@ -34,8 +38,8 @@ def room_rates(request):
         room = Room.objects.get(id=int(json_data['room_id']))
         if request.user not in room.hotel.admins.all() and not request.user.is_superuser:
             raise UserNotAllowed
-        # find settlements keys in data
-        all_settlements, discount = [],[]
+            # find settlements keys in data
+        all_settlements, discount = [], []
         for k in json_data.keys():
             if k[0] == 's':
                 all_settlements.append(k)
@@ -61,7 +65,7 @@ def room_rates(request):
             for k in discount:
                 try:
                     discount_on_date = int(json_data[k][i])
-                    if discount_on_date < 1 or discount_on_date > 99 :
+                    if discount_on_date < 1 or discount_on_date > 99:
                         raise ValueError
                     d, created = Discount.objects.get_or_create(date=on_date, room=room)
                     d.discount = discount_on_date
@@ -73,7 +77,7 @@ def room_rates(request):
                     settlement_id = int(k[1:])
                     settlement = SettlementVariant.objects.get(id=settlement_id)
                     price = int(json_data[k][i])
-                    placeprice, created = PlacePrice.objects.get_or_create(date=on_date,settlement=settlement)
+                    placeprice, created = PlacePrice.objects.get_or_create(date=on_date, settlement=settlement)
                     placeprice.amount = price
                     placeprice.currency = currency
                     placeprice.save()
@@ -82,7 +86,7 @@ def room_rates(request):
         payload = {'success': True}
     except UserNotAllowed:
         payload = {'success': False}
-    except :
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
 
@@ -92,16 +96,17 @@ def room_variants(request):
         room = Room.objects.get(id=request.REQUEST['room_id'])
         if request.user not in room.hotel.admins.all() and not request.user.is_superuser:
             raise UserNotAllowed
-        settlements = SettlementVariant.objects.filter(room=room,enabled=True).order_by('settlement')
+        settlements = SettlementVariant.objects.filter(room=room, enabled=True).order_by('settlement')
         results = []
         for s in settlements:
             results.append(s.settlement)
-        payload = {'success': True, 'settlements':results}
+        payload = {'success': True, 'settlements': results}
     except UserNotAllowed:
-        payload = {'success': False, 'error_msg':_('You are not allowed change room variants.')}
-    except :
+        payload = {'success': False, 'error_msg': _('You are not allowed change room variants.')}
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
+
 
 def room_delete(request, pk):
     try:
@@ -111,10 +116,11 @@ def room_delete(request, pk):
         room.delete()
         payload = {'success': True}
     except UserNotAllowed:
-        payload = {'success': False, 'error_msg':_('You are not allowed change room variants.')}
-    except :
+        payload = {'success': False, 'error_msg': _('You are not allowed change room variants.')}
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
+
 
 def request_hotel_delete(request, pk):
     try:
@@ -124,8 +130,8 @@ def request_hotel_delete(request, pk):
         req_hotel.delete()
         payload = {'success': True}
     except UserNotAllowed:
-        payload = {'success': False, 'error_msg':_('You are not allowed change room variants.')}
-    except :
+        payload = {'success': False, 'error_msg': _('You are not allowed change room variants.')}
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
 
@@ -138,18 +144,19 @@ def get_booking_amount(request):
         to_date = convert_to_date(request.REQUEST['to'])
         room = Room.objects.get(id=room_id)
         settlement = SettlementVariant.objects.filter(room=room, settlement=s)
-        delta = to_date-from_date
+        delta = to_date - from_date
         all_amount = 0
         on_date = from_date
         while on_date < to_date:
-            price = PlacePrice.objects.get(settlement=settlement, date = on_date)
-            all_amount +=int(price.amount)
-            on_date = on_date+timedelta(days=1)
-        all_amount = str(all_amount)+price.currency.code
-        payload = {'success': True, 'dayscount':delta.days, 'amount':all_amount}
-    except :
+            price = PlacePrice.objects.get(settlement=settlement, date=on_date)
+            all_amount += int(price.amount)
+            on_date = on_date + timedelta(days=1)
+        all_amount = str(all_amount) + price.currency.code
+        payload = {'success': True, 'dayscount': delta.days, 'amount': all_amount}
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
+
 
 def hotel_add(request):
     try:
@@ -168,7 +175,7 @@ def hotel_add(request):
         starcount = request.REQUEST['starcount']
         try:
             city = City.objects.get(name=c)
-        except :
+        except:
             city = City(name=c)
             city.save()
         hotel = Hotel()
@@ -185,22 +192,23 @@ def hotel_add(request):
         hotel.save()
         location = reverse('cabinet_info', args=[hotel.city.slug, hotel.slug])
         RequestAddHotel.objects.get(id=request_id).delete()
-        payload = {'success': True, 'location':location}
+        payload = {'success': True, 'location': location}
     except UserNotAllowed:
-        payload = {'success': False, 'error_msg':_('You are not allowed add hotel.')}
+        payload = {'success': False, 'error_msg': _('You are not allowed add hotel.')}
     return AjaxLazyAnswer(payload)
+
 
 def client_review(request, pk):
     try:
         hotel = Hotel.objects.get(id=pk)
-#        guests = Booking.objects.filter(hotel=hotel,to_date__gte=datetime.now()).values_list('user', flat=True)
+        #        guests = Booking.objects.filter(hotel=hotel,to_date__gte=datetime.now()).values_list('user', flat=True)
         if request.user.is_superuser:
             message = _('You are superuser and may add review.')
         elif request.user.pk in hotel.complete_booking_users_id:
             message = _('Thanks for you review!')
         else:
             raise UserNotAllowed
-        if Review.objects.filter(hotel=hotel,user=request.user).count():
+        if Review.objects.filter(hotel=hotel, user=request.user).count():
             raise UserNotAllowed
         food = request.REQUEST['point_food']
         service = request.REQUEST['point_service']
@@ -218,10 +226,11 @@ def client_review(request, pk):
         r.prices = prices
         r.review = review
         r.save()
-        payload = {'success': True,'message':message}
+        payload = {'success': True, 'message': message}
     except UserNotAllowed:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
+
 
 def tourism_places(request):
     try:
@@ -233,14 +242,15 @@ def tourism_places(request):
                 icon = tourism.category.icon.url
             else:
                 icon = ''
-            answer = {'name':tourism.get_name, 'latitude':tourism.latitude,
-                      'category':tourism.category.name,'category_id':tourism.category.pk,
-                      'longitude':tourism.longitude,'icon':icon, 'id':tourism.pk }
+            answer = {'name': tourism.get_name, 'latitude': tourism.latitude,
+                      'category': tourism.category.name, 'category_id': tourism.category.pk,
+                      'longitude': tourism.longitude, 'icon': icon, 'id': tourism.pk}
             results.append(answer)
-        payload = {'success': True, 'tourism':results}
-    except :
+        payload = {'success': True, 'tourism': results}
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
+
 
 def hotels_in_city(request):
     try:
@@ -248,43 +258,46 @@ def hotels_in_city(request):
         city = City.objects.get(pk=c)
         results = []
         for hotel in Hotel.objects.filter(city=city).order_by('starcount'):
-            answer = {'name':hotel.get_name, 'latitude':hotel.latitude,'url':hotel.get_absolute_url(),
-                      'address':hotel.address,'id':hotel.pk,'starcount':hotel.starcount,
-                      'img':make_thumbnail(hotel.main_image,width=113,height=75,aspect=1),
-                      'longitude':hotel.longitude, 'starcount_name':hotel.get_starcount_display(),
-                      'amount':str(int(hotel.current_amount))
+            answer = {'name': hotel.get_name, 'latitude': hotel.latitude, 'url': hotel.get_absolute_url(),
+                      'address': hotel.address, 'id': hotel.pk, 'starcount': hotel.starcount,
+                      'img': make_thumbnail(hotel.main_image, width=113, height=75, aspect=1),
+                      'longitude': hotel.longitude, 'starcount_name': hotel.get_starcount_display(),
+                      'amount': str(int(hotel.current_amount))
             }
 
             results.append(answer)
-        payload = {'success': True, 'hotels':results}
-    except :
+        payload = {'success': True, 'hotels': results}
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
+
 
 def hotels_in_country(request):
     try:
         results = []
         for hotel in Hotel.objects.all().order_by('starcount'):
-            answer = {'name':hotel.get_name, 'latitude':hotel.latitude,'url':hotel.get_absolute_url(),
-                      'address':hotel.address,'id':hotel.pk,'starcount':hotel.starcount,
-                      'img':make_thumbnail(hotel.main_image,width=113,height=75,aspect=1),
-                      'longitude':hotel.longitude, 'starcount_name':hotel.get_starcount_display(),
-                      'amount':str(int(hotel.current_amount ))}
+            answer = {'name': hotel.get_name, 'latitude': hotel.latitude, 'url': hotel.get_absolute_url(),
+                      'address': hotel.address, 'id': hotel.pk, 'starcount': hotel.starcount,
+                      'img': make_thumbnail(hotel.main_image, width=113, height=75, aspect=1),
+                      'longitude': hotel.longitude, 'starcount_name': hotel.get_starcount_display(),
+                      'amount': str(int(hotel.current_amount))}
             results.append(answer)
-        payload = {'success': True, 'hotels':results}
-    except :
+        payload = {'success': True, 'hotels': results}
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
+
 
 def payment_method(request):
     try:
         p_m = request.REQUEST['p_m']
         payment_method = PaymentMethod.objects.get(pk=p_m)
-        payload = {'success': True, 'id':payment_method.pk,'description':payment_method.description,
-                   'card':payment_method.use_card}
-    except :
+        payload = {'success': True, 'id': payment_method.pk, 'description': payment_method.description,
+                   'card': payment_method.use_card}
+    except:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
+
 
 def add_category(request):
     try:
@@ -293,14 +306,14 @@ def add_category(request):
         if request.user not in hotel.admins.all() and not request.user.is_superuser:
             raise UserNotAllowed
         category_name = request.REQUEST['category_name']
-        r = Room.objects.filter(hotel=hotel,name=category_name).count()
+        r = Room.objects.filter(hotel=hotel, name=category_name).count()
         if r > 0:
             raise UserNotAllowed
-        room = Room(hotel=hotel,name=category_name)
+        room = Room(hotel=hotel, name=category_name)
         room.save()
         file_path = get_image_attach_url(room)
         form_path = reverse('cabinet_room', args=[hotel.city.slug, hotel.slug, room.pk])
-        payload = {'success': True, 'file_path':file_path,'form_path':form_path }
+        payload = {'success': True, 'file_path': file_path, 'form_path': form_path}
     except UserNotAllowed:
         payload = {'success': False}
     return AjaxLazyAnswer(payload)
