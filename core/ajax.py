0d6606067dd98efcf072e55e8f6390e2e5f79979
nnmware <nnmware@gmail.com>
2013-02-25 15:20:07 +1100

    Ajax Uploader refactored

--- core/ajax.py
+++ core/ajax.py
@@ -824,9 +824,8 @@ class AjaxUploader(object):
             # try:
             #     filename = upload.read.im_self.META["HTTP_X_FILE_NAME"]
             # except KeyError:
-            raise request.REQUEST.items()
             if 1>0: #try:
-                filename = request.REQUEST['qqfile']
+                filename = request.FILES['qqfile'].name #request.REQUEST['qqfile']
             # except:
             #     return dict(success=False, error=_("AJAX request not valid"))
         else:
