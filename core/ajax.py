196203f6ecd91571fd346499e6f7301de1359024
nnmware <nnmware@gmail.com>
2013-03-18 03:15:43 +1100

    Dossier module changed

--- core/ajax.py
+++ core/ajax.py
@@ -692,12 +692,13 @@ def ajax_image_crop(request):
             im = im.convert('RGB')
         im.save(img)
         pic.save()
+        remove_thumbnails(pic.path)
         payload = {'success': True, 'id': pic.pk}
     except AccessError:
         payload = {'success': False, 'error': _('You are not allowed change this image')}
     except:
         payload = {'success': False}
-    return AjaxLazyAnswerNoCache(payload)
+    return AjaxLazyAnswer(payload)
 
 
 def comment_add(request, content_type, object_id, parent_id=None):
