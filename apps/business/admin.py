8c6182681bf03cb85cb274d5c5dbcb546e3a52f8
nnmware <nnmware@gmail.com>
2013-03-21 18:27:42 +1100

    Dossier module changed

--- apps/business/admin.py
+++ apps/business/admin.py
@@ -3,31 +3,36 @@ from nnmware.apps.business.models import *
 from django.utils.translation import ugettext_lazy as _
 from nnmware.core.admin import TypeBaseAdmin, BaseSkillInline
 
+
 class TypeEmployerProfileAdmin(TypeBaseAdmin):
     list_display = ('name', 'employer_type', 'is_radio')
     list_filter = ('name',)
     search_fields = ('name',)
-    ordering = ('name',)
-    fieldsets = ((_("Type of employer profile"), {"fields": [('name','employer_type','is_radio'),]}),)
+    ordering = ('-order_in_list', 'name')
+    fieldsets = ((_("Type of employer profile"), {"fields": [('name', 'employer_type', 'is_radio'), ]}),)
+
 
 class TypeEmployerOtherAdmin(TypeBaseAdmin):
-    list_display = ('name','employer_type')
+    list_display = ('name', 'employer_type')
     list_filter = ('name',)
     search_fields = ('name',)
     ordering = ('name',)
-    fieldsets = ((_("Type of other employer sphere"), {"fields": [('name','employer_type'),]}),)
+    fieldsets = ((_("Type of other employer sphere"), {"fields": [('name', 'employer_type'), ]}),)
+
 
 class TypeEmployerAdmin(TypeBaseAdmin):
-    list_display = ('name','order_in_list')
+    list_display = ('name', 'order_in_list')
     fieldsets = (
-        (_("Type of employer"), {"fields": [('name','order_in_list'),
-                                            ]}),)
-    ordering = ('-order_in_list','name',)
+        (_("Type of employer"), {"fields": [('name', 'order_in_list'),
+        ]}),)
+    ordering = ('-order_in_list', 'name',)
+
 
 class AgencyAdmin(TypeBaseAdmin):
     fieldsets = (
         (_("Agency name"), {"fields": [('name'),
-                                       ]}),)
+        ]}),)
+
 
 admin.site.register(Agency, AgencyAdmin)
 admin.site.register(TypeEmployer, TypeEmployerAdmin)
