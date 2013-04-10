7f724bbfcf5079535c6d7d9fd4ef2fb144064130
nnmware <nnmware@gmail.com>
2013-04-11 04:43:28 +1100

    Business block fixed

--- apps/address/ajax.py
+++ apps/address/ajax.py
@@ -3,25 +3,30 @@ from django.db.models.query_utils import Q
 from nnmware.apps.address.models import City, Country, Region, StationMetro
 from nnmware.core.ajax import AjaxLazyAnswer
 
+
 def base_autocomplete(obj, request):
     search_qs = obj.objects.filter(
         Q(name__icontains=request.REQUEST['q']) |
         Q(name_en__icontains=request.REQUEST['q'])).order_by('name')
     results = []
     for r in search_qs:
-        userstring = {'name': r.get_name, 'slug': r.slug }
+        userstring = {'name': r.get_name, 'slug': r.slug}
         results.append(userstring)
     payload = {'answer': results}
     return AjaxLazyAnswer(payload)
 
+
 def autocomplete_city(request):
     return base_autocomplete(City, request)
+
 
 def autocomplete_country(request):
     return base_autocomplete(Country, request)
 
+
 def autocomplete_region(request):
     return base_autocomplete(Region, request)
+
 
 def autocomplete_stationmetro(request):
     return base_autocomplete(StationMetro, request)
