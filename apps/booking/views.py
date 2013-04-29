85960930a7309a7b317938985c0a1423a80e4984
nnmware <nnmware@gmail.com>
2013-04-29 19:40:50 +1100

    Business block fixed

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -111,6 +111,7 @@ class HotelList(AjaxViewMixin, RedirectHttpView, ListView):
     model = Hotel
     template_name = "hotels/list.html"
     search = 0
+    payload = {}
 
     def get_queryset(self):
         key = sha1('%s:%s' % (get_session_from_request(self.request), self.request.get_full_path())).hexdigest()
@@ -248,7 +249,6 @@ class HotelList(AjaxViewMixin, RedirectHttpView, ListView):
 
     def get_context_data(self, **kwargs):
     # Call the base implementation first to get a context
-        self.payload = {}
         context = super(HotelList, self).get_context_data(**kwargs)
         context['title_line'] = _('list of hotels')
         context['tab'] = self.tab
