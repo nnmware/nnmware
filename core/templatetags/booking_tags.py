8d51e5a12d987ff7de9a596bd10071bc6f2da435
nnmware <nnmware@gmail.com>
2013-05-14 21:06:28 +1100

    Business block fixed

--- core/templatetags/booking_tags.py
+++ core/templatetags/booking_tags.py
@@ -226,7 +226,8 @@ def room_full_amount(context, room, rate):
     from_date = convert_to_date(f_date)
     to_date = convert_to_date(t_date)
     delta = (to_date - from_date).days
-    result = PlacePrice.objects.filter(date__range=(from_date, to_date)).aggregate(Sum('amount'))['amount__sum']
+    result = PlacePrice.objects.filter(settlement__room=room,
+                                       date__range=(from_date, to_date)).aggregate(Sum('amount'))['amount__sum']
     # room_all_amount = 0
     # for single_date in daterange(from_date, to_date):
     #     room_all_amount += room.amount_on_date_guest_variant(single_date, guests)[0]
