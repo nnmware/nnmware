49fb74d09431bd64793d260638bcbd54c8c2d2a7
nnmware <nnmware@gmail.com>
2013-05-20 04:19:16 +1100

    Booking module refactored

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -1004,7 +1004,7 @@ class ClientBooking(RedirectHttpsView, DetailView):
             if from_date > to_date:
                 f_date, t_date = t_date, f_date
                 from_date, to_date = to_date, from_date
-            if (from_date - datetime.now()).days < -1 or from_date == to_date:
+            if (from_date - datetime.now()).days < -1 or (from_date == to_date):
                 raise Http404
             avail_count = Availability.objects.filter(room=room, date__range=(from_date, to_date - timedelta(days=1)),
                                                       placecount__gt=0).count()
