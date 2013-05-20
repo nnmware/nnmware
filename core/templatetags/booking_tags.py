3bd9a7f23274b5973184d886c09cf3dabaef3c5e
nnmware <nnmware@gmail.com>
2013-05-21 10:13:07 +1100

    Booking code cleaned and optimized

--- core/templatetags/booking_tags.py
+++ core/templatetags/booking_tags.py
@@ -353,7 +353,7 @@ def hotels_city_count(slug):
 
 @register.assignment_tag
 def settlement_prices_on_dates(settlement, dates):
-    prices = PlacePrice.objects.filter(settlement=settlement, date__in=dates).values('date', 'amount').\
+    prices = PlacePrice.objects.filter(settlement=settlement, date__in=dates).values_list('date', 'amount').\
         order_by('date')
 #    result = {}
     # for d in dates:
