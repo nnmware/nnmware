8dd9ef7551f9884491dccad2116cbb9c9b497673
nnmware <nnmware@gmail.com>
2013-05-21 10:14:05 +1100

    Booking code cleaned and optimized

--- core/templatetags/booking_tags.py
+++ core/templatetags/booking_tags.py
@@ -358,10 +358,10 @@ def settlement_prices_on_dates(settlement, dates):
 #    result = {}
     # for d in dates:
     #     result[d.strftime("%d%m%Y")] = '1'
-    result = dict((d.strftime("%d%m%Y"), '1') for d in dates)
+    result = dict((d.strftime("%Y-%m-%d"), '1') for d in dates)
     for k, v in prices:
-        raise ImportError, k
-        result[k.strftime("%d%m%Y")] = v
+#        raise ImportError, k
+        result[k] = v
     return result
 
 
