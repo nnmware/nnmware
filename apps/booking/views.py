b67a58004283da9f126e10d81f9b9d9d1e67c8ab
nnmware <nnmware@gmail.com>
2013-05-23 19:25:39 +1100

    Booking code cleaned and optimized

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -128,6 +128,7 @@ class HotelList(RedirectHttpView, ListView):
     search = 0
 
     def get_queryset(self):
+        raise ImportError, self.request.get_full_path()
         key = sha1('%s' % (self.request.get_full_path(),)).hexdigest()
         result = []
         searched_date = False
