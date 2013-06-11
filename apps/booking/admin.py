afbf41467261dff311211508d04447e2c205e1cb
nnmware <nnmware@gmail.com>
2013-06-11 23:53:47 +1100

    version 0.7

--- apps/booking/admin.py
+++ apps/booking/admin.py
@@ -133,8 +133,8 @@ class BookingAdmin(admin.ModelAdmin):
     search_fields = ('from_date',)
     readonly_fields = ('uuid', 'ip', 'user_agent', 'currency')
     fieldsets = (
-        (_("Booking Event"), {"fields": [('ip', 'user_agent')]}),
-                                         # ("user", 'settlement', 'hotel'),
+        (_("Booking Event"), {"fields": [
+                                          ("user", 'settlement', 'hotel')]}),
                                          # ('from_date', 'to_date', 'status'),
                                          # ('last_name', 'first_name', 'middle_name'),
                                          # ('phone', 'email'),
