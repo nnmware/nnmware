2b1c5e791d906a6e8004111c5e0b0bb65101a115
nnmware <nnmware@gmail.com>
2013-06-12 00:37:38 +1100

    version 0.7

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -419,7 +419,7 @@ class SettlementVariant(models.Model):
 
     def __str__(self):
 #        if 1>0: #try:
-        return "Settlement -> %(settlement)s :: %(places)s" % {'settlement': self.settlement, 'places': self.room.places}
+        return "Settlement -> %s :: %s" % (self.settlement, self.room.places)
         # try:
         #     return _("Settlement -> %(settlement)s in %(room)s :: %(places)s :: %(hotel)s") % {
         #         'settlement': self.settlement, 'room': self.room.name, 'places': self.room.places,
