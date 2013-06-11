0ee4a82152888eb8e5b5c8738b68316523cdd443
nnmware <nnmware@gmail.com>
2013-06-11 23:50:01 +1100

    version 0.7

--- apps/booking/admin.py
+++ apps/booking/admin.py
@@ -143,21 +143,21 @@ class BookingAdmin(admin.ModelAdmin):
         (_("Credit card"), {"classes": ("grp-collapse grp-closed",), "fields": [("card_number", 'card_valid'),
                                                                                 ('card_holder', 'card_cvv2')]}),
     )
-    no_root_fieldsets = (
-        (_("Booking Event"), {"fields": [("user", 'settlement', 'hotel'),
-                                         ('from_date', 'to_date', 'status'),
-                                         ('last_name', 'first_name', 'middle_name'),
-                                         ('phone', 'email'),
-                                         ('amount', 'currency', 'date'),
-                                         ('uuid',),
-                                         ('ip', 'user_agent')
-        ]}),
-    )
-
-    def get_fieldsets(self, request, obj=None):
-        if request.user.username != 'root':
-            return self.no_root_fieldsets
-        return self.fieldsets
+    # no_root_fieldsets = (
+    #     (_("Booking Event"), {"fields": [("user", 'settlement', 'hotel'),
+    #                                      ('from_date', 'to_date', 'status'),
+    #                                      ('last_name', 'first_name', 'middle_name'),
+    #                                      ('phone', 'email'),
+    #                                      ('amount', 'currency', 'date'),
+    #                                      ('uuid',),
+    #                                      ('ip', 'user_agent')
+    #     ]}),
+    # )
+    #
+    # def get_fieldsets(self, request, obj=None):
+    #     if request.user.username != 'root':
+    #         return self.no_root_fieldsets
+    #     return self.fieldsets
 
 
 class RequestAddHotelAdmin(admin.ModelAdmin):
