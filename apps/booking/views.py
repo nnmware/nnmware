0b4397f56e88c6ecf042b4373bc27d545d2cdb50
nnmware <nnmware@gmail.com>
2013-06-20 02:01:01 +1100

    version 0.7

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -195,7 +195,7 @@ class HotelList(AjaxViewMixin, RedirectHttpView, ListView):
                 need_days = (to_date - from_date).days
                 date_period = (from_date, to_date - timedelta(days=1))
                 searched_hotels_list = Availability.objects.filter(room__pk__in=rooms_list, date__in=date_period,
-                    min_days__lte=need_days, placecount__gt=0).annotate(num_days=Count('room')).\
+                    min_days__lte=need_days, placecount__gt=0).annotate(num_days=Sum('room')).\
                     filter(num_days__gte=need_days).order_by('room__hotel').values_list('room__hotel__pk',
                                                                                         flat=True).distinct()
                 search_hotel = search_hotel.filter(pk__in=searched_hotels_list, work_on_request=False)
@@ -326,7 +326,7 @@ class HotelDetail(AjaxViewMixin, HotelPathMixin, AttachedImagesMixin, DetailView
                     room__hotel=self.object).values_list('room__id', flat=True).distinct()
                 date_period = (from_date, to_date - timedelta(days=1))
                 searched_room_list = Availability.objects.filter(room__pk__in=rooms_list, date__range=date_period,
-                    min_days__lte=need_days, placecount__gt=0).annotate(num_days=Count('room')).\
+                    min_days__lte=need_days, placecount__gt=0).annotate(num_days=Sum('room')).\
                     filter(num_days__gte=need_days).order_by('room').values_list('room__pk', flat=True).distinct()
                 room_with_amount_list = PlacePrice.objects.filter(settlement__room__pk__in=rooms_list,
                     date__range=date_period, amount__gte=0).annotate(num_days=Sum('settlement__room')).\
