723a40c1a52318b2855ad6d638aa140540f69d2d
nnmware <nnmware@gmail.com>
2013-06-29 00:40:08 +1100

    version 0.7

--- core/decorators.py
+++ core/decorators.py
@@ -58,7 +58,7 @@ def ajax_request(func):
 
 def ssl_required(view_func):
     def _checkssl(request, *args, **kwargs):
-        if not request.is_secure() and not settings.DEBUG:
+        if not request.is_secure() and not settings.DEBUG and not settings.NOHTTPS:
             if hasattr(settings, 'SSL_DOMAIN'):
                 url_str = urlparse.urljoin(
                     settings.SSL_DOMAIN,
