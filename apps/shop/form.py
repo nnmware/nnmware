b0f39f0aaa53e849d813791035b4fb2612bbeebf
nnmware <nnmware@gmail.com>
2013-07-19 23:41:50 +1100

    Fixed booking app

--- apps/shop/form.py
+++ apps/shop/form.py
@@ -5,59 +5,63 @@ from django.utils.translation import ugettext_lazy as _
 from nnmware.apps.shop.models import Product, ProductParameter, Order, ProductColor, ProductMaterial, ProductCategory
 from nnmware.apps.shop.models import DeliveryMethod
 
+
 class EditProductForm(forms.ModelForm):
     """
     Superuser edit product form
     """
     params = forms.ModelChoiceField(queryset=ProductParameter.objects.all(), required=False)
+
     class Meta:
         model = Product
-        fields = ('name','category','slug','amount','quantity','avail','description',
-            'shop_pn','vendor_pn','vendor','latest','teaser','discount','bestseller')
+        fields = ('name', 'category', 'slug', 'amount', 'quantity', 'avail', 'description',
+                  'shop_pn', 'vendor_pn', 'vendor', 'latest', 'teaser', 'discount', 'bestseller')
 
     def clean(self):
         return self.cleaned_data
+
 
 class EditProductFurnitureForm(forms.ModelForm):
     colors = forms.ModelChoiceField(queryset=ProductColor.objects.all(), required=False)
     materials = forms.ModelChoiceField(queryset=ProductMaterial.objects.all(), required=False)
     category = forms.ModelChoiceField(queryset=ProductCategory.objects.exclude(rootnode=True))
     related_products = forms.ModelChoiceField(queryset=Product.objects.all(), required=False)
+
     class Meta:
         model = Product
-        fields = ('name','category','slug','amount','discount_percent','avail','description',
-                  'vendor','width','height','depth','on_main','latest','bestseller')
+        fields = ('name', 'category', 'slug', 'amount', 'discount_percent', 'avail', 'description',
+                  'vendor', 'width', 'height', 'depth', 'on_main', 'latest', 'bestseller')
+
 
 class OrderStatusForm(forms.ModelForm):
-
     class Meta:
         model = Order
         fields = ('status',)
 
-class OrderCommentForm(forms.ModelForm):
 
+class OrderCommentForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('comment',)
 
-class OrderTrackingForm(forms.ModelForm):
 
+class OrderTrackingForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('tracknumber','cargoservice')
+        fields = ('tracknumber', 'cargoservice')
+
 
 class AnonymousUserOrderAddForm(forms.ModelForm):
-
     class Meta:
         model = Order
-        fields = ('first_name', 'last_name','email', 'phone','address','buyer_comment', 'delivery')
+        fields = ('first_name', 'last_name', 'email', 'phone', 'address', 'buyer_comment', 'delivery')
 
     def __init__(self, *args, **kwargs):
         super(AnonymousUserOrderAddForm, self).__init__(*args, **kwargs)
         self.fields['delivery'].queryset = DeliveryMethod.objects.all()
 
     def clean_first_name(self):
-        first_name =  self.cleaned_data['first_name']
+        first_name = self.cleaned_data['first_name']
         if first_name:
             return first_name
         raise forms.ValidationError(_("Required field"))
@@ -92,8 +96,8 @@ class AnonymousUserOrderAddForm(forms.ModelForm):
             return delivery
         raise forms.ValidationError(_("Required field"))
 
-class RegisterUserOrderAddForm(forms.ModelForm):
 
+class RegisterUserOrderAddForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('buyer_comment', 'delivery')
