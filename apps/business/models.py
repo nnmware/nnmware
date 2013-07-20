b7b3cdc3b4c38fd9925240fd0354663b6e387c69
nnmware <nnmware@gmail.com>
2013-07-21 00:19:31 +1100

    Fixed booking app

--- apps/business/models.py
+++ apps/business/models.py
@@ -161,8 +161,8 @@ class CompanyCategory(Tree):
 
 
 class Company(AbstractName, AbstractLocation, MetaGeo, AbstractWTime, AbstractDate):
-    admins = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_('Company Admins'),
-                                    null=True, blank=True, related_name='%(class)s_comp_adm')
+    # admins = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_('Company Admins'),
+    #                                 null=True, blank=True, related_name='%(class)s_comp_adm')
     main_category = models.ForeignKey(CompanyCategory, blank=True, null=True, verbose_name=_('Company category'),
                                       related_name='company', on_delete=models.SET_NULL)
     category = models.ManyToManyField(CompanyCategory, verbose_name=_('All company category'),
