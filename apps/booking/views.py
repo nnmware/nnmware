fe32993efdc4c82fbff1e9ba5e023404ddda037d
nnmware <nnmware@gmail.com>
2013-07-27 01:53:36 +1100

    Small fixes

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -850,8 +850,6 @@ class ReportView(CurrentUserSuperuser, ListView):
         self.report_arg = report_type
         if result:
             self.full_count = result.count()
-        else:
-            self.full_count = 0
         return result
 
     def get_context_data(self, **kwargs):
