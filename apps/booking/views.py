32653381ef4ae4a93e2ff3d87ca9e2768e1518e4
nnmware <nnmware@gmail.com>
2013-07-27 00:21:13 +1100

    Small fixes

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -809,7 +809,7 @@ class ReportView(CurrentUserSuperuser, ListView):
         elif report_type == 'non-correct':
             noncorrect = []
             not_filled_room = Room.objects.filter(availability__date__range=(datetime.now(),
-                datetime.now() + timedelta(days=13)), availability__placecount__gt=0).annotate(num_days=Count('pk')).filter(num_days__lt=14).\
+                datetime.now() + timedelta(days=13))).annotate(num_days=Count('pk')).filter(num_days__lt=14).\
                 order_by('hotel').values_list('hotel__pk', flat=True).distinct()
             not_filled_amount = SettlementVariant.objects.filter(enabled=True, placeprice__date=datetime.now(),
                 placeprice__amount=0).order_by('room__hotel').values_list('room__hotel__pk', flat=True).distinct()
