3e7d43e4b1adbc760980a0cd9fae627b7770c73a
nnmware <nnmware@gmail.com>
2013-07-27 00:08:40 +1100

    Small fixes

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -811,8 +811,8 @@ class ReportView(CurrentUserSuperuser, ListView):
             not_filled_room = Room.objects.filter(availability__date__range=(datetime.now(),
                 datetime.now() + timedelta(days=13))).annotate(num_days=Count('pk')).filter(num_days__lt=14).\
                 order_by('hotel').values_list('hotel__pk', flat=True).distinct()
-            # not_filled_amount = SettlementVariant.objects.filter(enabled=True, placeprice__date=datetime.now(),
-            #     placeprice__amount=0).order_by('room__hotel').values_list('room__hotel__pk', flat=True).distinct()
+            not_filled_amount = SettlementVariant.objects.filter(enabled=True, placeprice__date=datetime.now(),
+                placeprice__amount=0).order_by('room__hotel').values_list('room__hotel__pk', flat=True).distinct()
             # for hotel in Hotel.objects.exclude(admins=None).exclude(work_on_request=True):
             #     in_report = False
             #     for room in hotel.room_set.all():
@@ -826,7 +826,7 @@ class ReportView(CurrentUserSuperuser, ListView):
             #     if in_report:
             #         noncorrect.append(hotel.pk)
             result = Hotel.objects.select_related().exclude(admins=None).exclude(work_on_request=True).\
-                filter(Q(pk__in=not_filled_room)) # | Q(pk__in=not_filled_amount))
+                filter(Q(pk__in=not_filled_room) | Q(pk__in=not_filled_amount))
             self.report_name = _('Hotels, not fully entered info')
         elif report_type == 'nullpercent':
             result = Hotel.objects.select_related().filter(agentpercent__date__lte=datetime.now()).\
