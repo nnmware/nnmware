7c40ae4c1b8b8072ae4547a9187408ae0e2e129d
nnmware <nnmware@gmail.com>
2013-07-27 19:43:00 +1100

    Transport sale app created

--- apps/transport/admin.py
+++ apps/transport/admin.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.contrib import admin
+from nnmware.apps.transport.models import VehicleColor
 
+from nnmware.core.admin import ColorAdmin
+
+admin.site.register(VehicleColor, ColorAdmin)
