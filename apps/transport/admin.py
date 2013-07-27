8550c4019037fb4601279c9138d697a02f5f1bbd
nnmware <nnmware@gmail.com>
2013-07-27 23:32:31 +1100

    Transport sale app created

--- apps/transport/admin.py
+++ apps/transport/admin.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
-from django.contrib import admin
-from nnmware.apps.transport.models import VehicleColor
 
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import admin
+from nnmware.apps.transport.models import VehicleColor, VehicleKind
 from nnmware.core.admin import ColorAdmin
 
+
+class VehicleKindAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order_in_list', 'slug')
+    fieldsets = (
+        (_("Type of employer"), {"fields": [('name', 'order_in_list'), ]}),)
+    ordering = ('-order_in_list', 'name',)
+
+
 admin.site.register(VehicleColor, ColorAdmin)
+admin.site.register(VehicleKind, VehicleKindAdmin)
