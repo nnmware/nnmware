cda8bb6f98110b20c19a67d0716db4b888f7e89b
nnmware <nnmware@gmail.com>
2013-07-27 23:48:01 +1100

    Transport sale app created

--- apps/transport/admin.py
+++ apps/transport/admin.py
@@ -2,11 +2,11 @@
 
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
-from nnmware.apps.transport.models import VehicleColor, VehicleKind
+from nnmware.apps.transport.models import VehicleColor, VehicleKind, VehicleTransmission, VehicleCarcass, VehicleEngine, VehicleFeature, VehicleMark
 from nnmware.core.admin import ColorAdmin
 
 
-class VehicleKindAdmin(admin.ModelAdmin):
+class VehicleBaseParamAdmin(admin.ModelAdmin):
     list_display = ('name', 'order_in_list', 'slug')
     fieldsets = (
         (_('Type of vehicle'), {"fields": [('name', 'order_in_list'),
@@ -16,5 +16,42 @@ class VehicleKindAdmin(admin.ModelAdmin):
     ordering = ('-order_in_list', 'name',)
 
 
+class VehicleKindAdmin(VehicleBaseParamAdmin):
+    pass
+
+
+class VehicleBaseParamForAdmin(VehicleBaseParamAdmin):
+    fieldsets = (
+        (_('Type of vehicle'), {"fields": [('name', 'order_in_list'), ('type_vehicles', ),
+                                ('description',)]}),
+        (_("English"), {"classes": ("grp-collapse grp-closed",),
+                        "fields": [("name_en",), ("description_en",), ]}),)
+
+
+class VehicleTransmissionAdmin(VehicleBaseParamForAdmin):
+    pass
+
+
+class VehicleCarcassAdmin(VehicleBaseParamForAdmin):
+    pass
+
+
+class VehicleEngineAdmin(VehicleBaseParamForAdmin):
+    pass
+
+
+class VehicleFeatureAdmin(VehicleBaseParamForAdmin):
+    pass
+
+
+class VehicleMarkAdmin(VehicleBaseParamForAdmin):
+    pass
+
+
 admin.site.register(VehicleColor, ColorAdmin)
 admin.site.register(VehicleKind, VehicleKindAdmin)
+admin.site.register(VehicleTransmission, VehicleTransmissionAdmin)
+admin.site.register(VehicleCarcass, VehicleCarcassAdmin)
+admin.site.register(VehicleEngine, VehicleEngineAdmin)
+admin.site.register(VehicleFeature, VehicleFeatureAdmin)
+admin.site.register(VehicleMark, VehicleMarkAdmin)
