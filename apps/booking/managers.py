ffab91510af342d8523e2a1f901cc10e0c8c6baf
nnmware <nnmware@gmail.com>
2013-09-03 07:56:37 +1100

    Remove deprecated get_query_set from manager

--- apps/booking/managers.py
+++ apps/booking/managers.py
@@ -3,5 +3,5 @@ from django.db.models.manager import Manager
 
 class SettlementVariantManager(Manager):
 
-    def get_query_set(self):
-        return super(SettlementVariantManager, self).get_query_set().filter(enabled=True).order_by('settlement')
+    def get_queryset(self):
+        return super(SettlementVariantManager, self).get_queryset().filter(enabled=True).order_by('settlement')
