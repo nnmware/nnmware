c3eca83212ce41d3a6ef0f73ea0a6fca24c5d8e3
nnmware <nnmware@gmail.com>
2013-09-14 21:31:05 +1100

    Follow template tag added

--- core/ajax.py
+++ core/ajax.py
@@ -497,8 +497,8 @@ def follow_unfollow(request, content_type, object_id):
     count = None
     if 1>0: #try:
         ctype = get_object_or_404(ContentType, id=int(content_type))
-        follow = Follow.objects.filter(user=request.user, content_type=ctype, object_id=int(object_id)).count()
-        actor = ctype.get_object_for_this_type(id=int(object_id))
+        follow = Follow.objects.filter(user=request.user, content_type=ctype, object_id=object_id).count()
+        actor = ctype.get_object_for_this_type(id=object_id)
         if not follow:
             follow(request.user, actor)
         else:
