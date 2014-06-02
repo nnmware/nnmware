340cdf33b6343c73f721a1cf24c52b68e2266337
nnmware <nnmware@gmail.com>
2014-06-02 22:25:39 +1100

    Add range template filter

--- core/templatetags/core.py
+++ core/templatetags/core.py
@@ -884,4 +884,15 @@ def multiply_2args(arg1, arg2):
     return result
 
 
+@register.filter
+def get_range(value):
+    """
+    Filter - returns a list containing range made from given value
+    Usage (in template):
+
+    <ul>{% for i in 3|get_range %}
+      <li>{{ i }}. Do something</li>
+    {% endfor %}</ul>
+    """
+    return range(value)
 
