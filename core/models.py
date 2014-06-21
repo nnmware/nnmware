f151fb687021d8f3c2a5e1f98a5137d2ab673a7c
nnmware <nnmware@gmail.com>
2014-06-22 07:42:18 +1100

    Fixed in user model

--- core/models.py
+++ core/models.py
@@ -694,16 +694,16 @@ class NnmwareUser(AbstractUser, AbstractImg):
         self.date_modified = now()
         super(NnmwareUser, self).save(*args, **kwargs)
 
-    def thumbnail(self):
-        if self.img:
-            path = self.img.url
-            tmb = make_thumbnail(path, height=60, width=60)
-            return '<a style="display:block;text-align:center;" target="_blank" href="%s"><img src="%s" /></a>' \
-                   '<p style="text-align:center;margin-top:5px;">%sx%s px</p>' % (path, tmb, self.avatar_width,
-                                                                                  self.avatar_height)
-        return "No image"
-    thumbnail.allow_tags = True
-    thumbnail.short_description = 'Thumbnail'
+    # def thumbnail(self):
+    #     if self.img:
+    #         path = self.img.url
+    #         tmb = make_thumbnail(path, height=60, width=60)
+    #         return '<a style="display:block;text-align:center;" target="_blank" href="%s"><img src="%s" /></a>' \
+    #                '<p style="text-align:center;margin-top:5px;">%sx%s px</p>' % (path, tmb, self.avatar_width,
+    #                                                                               self.avatar_height)
+    #     return "No image"
+    # thumbnail.allow_tags = True
+    # thumbnail.short_description = 'Thumbnail'
 
 
 class Like(AbstractLike):
