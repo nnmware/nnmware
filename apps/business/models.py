94855e7ba6b20af1f1ba9e01b25b25e7429138dc
nnmware <nnmware@gmail.com>
2014-08-14 05:58:16 +1100

    Cleanup core

--- apps/business/models.py
+++ apps/business/models.py
@@ -149,7 +149,7 @@ class AbstractEmployee(AbstractImg):
 
     @property
     def get_agent_avatar(self):
-        if self.agent_img is not None:
+        if self.agent_img is not None and self.agent_img != '':
             return self.agent_img.url
         return setting('DEFAULT_AVATAR', 'noavatar.png')
 
